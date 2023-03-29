ab87d048b74922317187fbb5b0687573c4400a3d
Adriane Felicio <adrianefelicios@outlook.com>
2023-03-28 21:17:03 -0300

    contaatt

--- conta/menu.java
+++ conta/menu.java
@@ -1,30 +1,65 @@
 package conta;
 
 import java.util.Scanner;
+//import conta.model.Conta;
+import conta.util.Cores;
+import conta.model.*;
+
 
 public class menu {
 
 	public static void main(String[] args) {
 		
+		//Teste da Classe Conta
+		
+		/*Conta c1 = new Conta (1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar(); */
+		
+		//Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123 ,1, "Mariana",  15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		//Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
+		
+		
 			Scanner input = new Scanner(System.in);
 			int opcao;
 			while (true) {
-				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-				System.out.println("                       Banco Generation                                  ");
-				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-				System.out.println(" * (1)  Criar conta             /  (2) Listar todas as contas           *");
-				System.out.println(" \n ");
-				System.out.println(" * (3) Buscar conta por número  / (4) Atualizar dados da conta          *");
-				System.out.println(" \n");
-				System.out.println(" * (5) Apagar conta             / (6) Sacar                             *");
-				System.out.println(" \n");
-				System.out.println(" * (7) Depositar                / (8) Transferir valores entre contas   *");
-				System.out.println("\n");
-				System.out.println(" *                       ****(9) Sair****                               *");
-				System.out.println(" \n");
-				System.out.println(" ********************** Digite a opção desejada  ************************");
-				System.out.println(" \n");
-				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				
+				System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_CYAN_BOLD_BRIGHT);
+				System.out.println("************************************************************************");
+				System.out.println("*                       Banco Generation                               *");
+				System.out.println("************************************************************************");
+				System.out.println("| (1)  Criar conta             |  (2) Listar todas as contas           |");
+				System.out.println("|                                                                      |");
+				System.out.println("| (3) Buscar conta por número  | (4) Atualizar dados da conta          |");
+				System.out.println("|                                                                      |");
+				System.out.println("| (5) Apagar conta             | (6) Sacar                             |");
+				System.out.println("|                                                                      |");
+				System.out.println("| (7) Depositar                | (8) Transferir valores entre contas   |");
+				System.out.println("|                                                                      |");
+				System.out.println("|************************** (9) Sair **********************************|");
+				System.out.println("|                                                                      |");
+				System.out.println("|********************* Digite a opção desejada  ***********************|");
+				System.out.println("|                                                                      |");
+				System.out.println("|______________________________________________________________________|");
+				System.out.println(Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_CYAN_BOLD);
 				opcao = input.nextInt();
 
 				if (opcao == 9) {
